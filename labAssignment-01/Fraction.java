42570a9ced020a467a9c1289c21eff1a9ba29ce8
Shahriar Swanon <115762022+shahriarSwanon@users.noreply.github.com>
2023-09-21 16:58:20 +0600

    Update Fraction.java

--- labAssignment-01/Fraction.java
+++ labAssignment-01/Fraction.java
@@ -34,11 +34,11 @@ public class Fraction {
         return s;
     }
     public void add(Fraction fraction){
-        double n1 = this.numerator;
-        double d1 = this.denominator;
-        double n2 = fraction.numerator;
-        double d2 = fraction.denominator;
-        double n,d;
+        int n1 = this.numerator;
+        int d1 = this.denominator;
+        int n2 = fraction.numerator;
+        int d2 = fraction.denominator;
+        int n,d;
         if(d1==d2)
         {
             n=n1+n2;
@@ -53,11 +53,11 @@ public class Fraction {
         System.out.println("Sum: "+n+" / "+d);
     }
     public void sub(Fraction fraction){
-        double n1 = this.numerator;
-        double d1 = this.denominator;
-        double n2 = fraction.numerator;
-        double d2 = fraction.denominator;
-        double n,d;
+        int n1 = this.numerator;
+        int d1 = this.denominator;
+        int n2 = fraction.numerator;
+        int d2 = fraction.denominator;
+        int n,d;
         if(d1==d2)
         {
             n=n1-n2;
@@ -71,21 +71,21 @@ public class Fraction {
         System.out.println("Sub: "+n+" / "+d);
     }
     public void multiplication(Fraction fraction){
-        double n1 = this.numerator;
-        double d1 = this.denominator;
-        double n2 = fraction.numerator;
-        double d2 = fraction.denominator;
-        double n,d;
+        int n1 = this.numerator;
+        int d1 = this.denominator;
+        int n2 = fraction.numerator;
+        int d2 = fraction.denominator;
+        int n,d;
         n=(n1*n2);
         d=(d1*d2);
         System.out.println("Multiplication: "+n+" / "+d);
     }
     public void division(Fraction fraction){
-        double n1 = this.numerator;
-        double d1 = this.denominator;
-        double n2 = fraction.numerator;
-        double d2 = fraction.denominator;
-        double n,d;
+        int n1 = this.numerator;
+        int d1 = this.denominator;
+        int n2 = fraction.numerator;
+        int d2 = fraction.denominator;
+        int n,d;
         n=(n1*d2);
         d=(d1*n2);
         System.out.println("Division: "+n+" / "+d);
